366bd0bbb55de7f566bb1395b0f2bd913fb85bf8
Guilherme Lino <guilhermelinosp@gmail.com>
2024-07-01 18:50:37 -0300

    1

--- insert_contratos_script.sh
+++ insert_contratos_script.sh
@@ -1,45 +1,42 @@
 #!/bin/bash
 
 # Definição das variáveis de conexão com o banco de dados
-DB_HOST="inpi.database.windows.net"
-DB_PORT="1433"
-DB_NAME="PROGRAMAS"
-DB_USER="inpi"
-DB_PASSWORD="!@#N3w0@"
-
-# Diretório onde estão localizados os arquivos XML
-XML_DIR="/home/ubuntu/INPI/PROGRAMAS"
+if [ ! -f .env ]; then
+  echo ".env file not found!"
+  exit 1
+else
+  export $(cat .env | xargs)
+fi
 
 # Verificar se o diretório existe e pode ser acessado
-if [ ! -d "$XML_DIR" ]; then
-    echo "Diretório PROGRAMAS não encontrado."
+if [ ! -d "$DIR_CONTRATOS" ]; then
+    echo "Diretório não encontrado."
     exit 1
 fi
 
-# Inicializar um array para armazenar os despachos
-declare -a despachos_array
-
 # Obter o último diretório com maior número
-maior_pasta=$(ls -d "$XML_DIR"/*/ | sort -n | tail -n 1)
+maior_pasta=$(ls -d "$DIR_CONTRATOS"/*/ | sort -n | tail -n 1)
 
 if [[ -n "$maior_pasta" ]]; then
-    # Iterar sobre todos os arquivos XML no diretório mais recente
-    for xml_file in "$maior_pasta"/*.xml; do
-        if [[ -f "$xml_file" ]]; then
-            # Extrair o conteúdo XML do arquivo usando xmlstarlet
-            xml_content=$(xmlstarlet sel -t -c "/despacho" "$xml_file")
+    # Encontrar o arquivo XML
+    xml_file=$(find "$maior_pasta" -type f -name "*.xml")
 
-            # Nome do diretório (Revista) e data atual (apenas data, sem hora) para inclusão na tabela
-            REVISTA=$(basename "$maior_pasta")
-            DATA=$(date +'%Y-%m-%d')
+    if [[ -f "$xml_file" ]]; then
+        # Nome do diretório (Revista) e data atual (apenas data, sem hora) para inclusão na tabela
+        REVISTA=$(basename "$maior_pasta")
+        DATA=$(date +'%Y-%m-%d')
 
+        # Extrair o conteúdo XML do arquivo usando xmllint
+        xmllint --xpath '//despacho' "$xml_file" | while IFS= read -r despacho; do
             # Chamar a procedure no SQL Server para enviar cada XML individualmente
-            /opt/mssql-tools/bin/sqlcmd -S "$DB_HOST,$DB_PORT" -d "$DB_NAME" -U "$DB_USER" -P "$DB_PASSWORD" \
-                -Q "EXEC SP_INSERT_XML '$REVISTA', '$DATA', '$xml_content';"
+            sqlcmd -S "$DB_HOST,$DB_PORT" -d "CONTRATOS" -U "$DB_USER" -P "$DB_PASSWORD" \
+                -Q "EXEC SP_INSERT_XML_CONTRATOS '$REVISTA', '$DATA', '$despacho';"
 
             echo "Inserção do XML $xml_file no banco de dados concluída."
-        fi
-    done
+        done
+    else
+        echo "Nenhum arquivo XML encontrado em $maior_pasta."
+    fi
 else
-    echo "Nenhum diretório encontrado em $XML_DIR."
+    echo "Nenhum diretório encontrado em $DIR_CONTRATOS."
 fi
