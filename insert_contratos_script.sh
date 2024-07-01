26a6f51043ceac4921c0de55ae7191b7d3391d26
Guilherme Lino <guilhermelinosp@gmail.com>
2024-07-01 18:58:07 -0300

    2

--- insert_contratos_script.sh
+++ insert_contratos_script.sh
@@ -1,36 +1,28 @@
 #!/bin/bash
 
-# Definição das variáveis de conexão com o banco de dados
 if [ ! -f .env ]; then
-  echo ".env file not found!"
+  echo ".ENV não encontrado"
   exit 1
 else
   export $(cat .env | xargs)
 fi
 
-# Verificar se o diretório existe e pode ser acessado
 if [ ! -d "$DIR_CONTRATOS" ]; then
     echo "Diretório não encontrado."
     exit 1
 fi
 
-# Obter o último diretório com maior número
 maior_pasta=$(ls -d "$DIR_CONTRATOS"/*/ | sort -n | tail -n 1)
 
 if [[ -n "$maior_pasta" ]]; then
-    # Encontrar o arquivo XML
     xml_file=$(find "$maior_pasta" -type f -name "*.xml")
 
     if [[ -f "$xml_file" ]]; then
-        # Nome do diretório (Revista) e data atual (apenas data, sem hora) para inclusão na tabela
-        REVISTA=$(basename "$maior_pasta")
-        DATA=$(date +'%Y-%m-%d')
+        xmllint --format "$xml_file" | xmllint --xpath '//despacho' - | while IFS= read -r despacho; do
+            despacho=$(echo "$despacho" | tr -d '\n' | tr -d '\r')
 
-        # Extrair o conteúdo XML do arquivo usando xmllint
-        xmllint --xpath '//despacho' "$xml_file" | while IFS= read -r despacho; do
-            # Chamar a procedure no SQL Server para enviar cada XML individualmente
             sqlcmd -S "$DB_HOST,$DB_PORT" -d "CONTRATOS" -U "$DB_USER" -P "$DB_PASSWORD" \
-                -Q "EXEC SP_INSERT_XML_CONTRATOS '$REVISTA', '$DATA', '$despacho';"
+                -Q "EXEC SP_INSERT_XML_CONTRATOS '$(basename "$maior_pasta")', '$(date +'%Y-%m-%d')', '$despacho';"
 
             echo "Inserção do XML $xml_file no banco de dados concluída."
         done
