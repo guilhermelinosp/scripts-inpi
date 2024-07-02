f9598b8f3414fdd672a7c73836e234d1df552e9c
Guilherme Lino <guilhermelinosp@gmail.com>
2024-07-01 21:13:19 -0300

    refac: испытание

--- insert_contratos_script.sh
+++ insert_contratos_script.sh
@@ -34,11 +34,11 @@ if [[ -n "$latestDirectory" ]]; then
             xmlContentEscaped=$(echo "${xmlContent}" | sed "s/'/''/g")
 
             # Example: Construct SQL command to insert XML content into a table
-            sql_command="INSERT INTO dbo.TB_XML (Revista, Data, XmlContent) VALUES ('${basename "${latestDirectory}"}', GETDATE(), '${xmlContentEscaped}')"
+# Correctly construct SQL command to call stored procedure with parameters
+            sql_command="EXEC dbo.SP_INSERT_XML_CONTRATOS '${basename "${latestDirectory}"}', '${xmlContentEscaped}'"
 
-            # Construct SQL connection string and execute SQL command using sqlcmd
+ # Construct SQL connection string and execute SQL command using sqlcmd
             sqlcmd -S "${DB_HOST},${DB_PORT}" -d CONTRATOS -U "${DB_USER}" -P "${DB_PASSWORD}" -Q "${sql_command}"
-
             if [ $? -eq 0 ]; then
                 echo "Inserted XML ${xmlFile} into database."
             else
